9307baef915b4720e602eb891956100c33dda3d6
Mykhailo <mikechud87@gmail.com>
2024-10-25 17:15:58 +0300

    fix: add the dropdown default options

--- resources/formUtils.js
+++ resources/formUtils.js
@@ -131,10 +131,11 @@ function ktPopulateDropdown($dropdown, selectedItemId, actionUrl) {
 	$spinner.show();
 	$dropdown.hide();
 	
+	// Clear previous options but keep the default option
+	$dropdown.find("option:not([value=''])").remove();
+	
 	$.getJSON(actionUrl)
 		.done((items) => {
-			$dropdown.empty();
-			
 			if (ktIsObject(items)) {
 				$.each(items, (id, name) => {
 					const optionLabel = ktGetOptionLabel(name, actionUrl);
@@ -142,18 +143,19 @@ function ktPopulateDropdown($dropdown, selectedItemId, actionUrl) {
 				});
 				
 				ktPreselectItems($dropdown, selectedItemId);
-				$dropdown.show();
 			} else {
-				ktShowError($dropdown, 'No valid items found');
+				// Display an error message option if no valid items are found
+				$dropdown.append($('<option>', { value: '', text: 'Error: No valid items found', disabled: true }));
 			}
 		})
 		.fail((jqXHR) => {
-			const errorMessage = ktGetErrorMessage(jqXHR);
+			const errorMessage = `Error: ${ktGetErrorMessage(jqXHR)}`;
 			console.error('Error:', errorMessage);
-			ktShowError($dropdown, `Error: ${errorMessage}`);
+			$dropdown.append($('<option>', { value: '', text: errorMessage, disabled: true }));
 		})
 		.always(() => {
 			$spinner.hide();
+			$dropdown.show();
 		});
 }
 
