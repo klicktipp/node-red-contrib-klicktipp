73278a5c59cd06638eff6a435e5a1377c8559185
Olex2324 <kovalenko.odev@gmail.com>
2025-04-16 11:32:52 +0300

    refactor(adjustErrorMessage help function): return error message instead of assign it to a variable and then return

--- nodes/utils/adjustErrorMessage.js
+++ nodes/utils/adjustErrorMessage.js
@@ -1,63 +1,40 @@
 function adjustErrorMessage(error) {
-	let errorMessage = '';
 	switch (error) {
 		case 4:
-			errorMessage =
-				'The email address is unsubscribed. You cannot re-subscribe an email address if the contact has unsubscribed.';
-			break;
+			return 'The email address is unsubscribed. You cannot re-subscribe an email address if the contact has unsubscribed.';
 		case 5:
-			errorMessage = 'Invalid email address.';
-			break;
+			return 'Invalid email address.';
 		case 6:
-			errorMessage = 'There was an error sending the confirmation email.';
-			break;
+			return 'There was an error sending the confirmation email.';
 		case 7:
-			errorMessage = 'Email address not found.';
-			break;
+			return 'Email address not found.';
 		case 8:
-			errorMessage =
-				'Invalid value in custom field. The provided value is not valid for the field type.';
-			break;
+			return 'Invalid value in custom field. The provided value is not valid for the field type.';
 		case 9:
-			errorMessage =
-				'The SMS number is already assigned to another contact. If you subscribe an email address and add a phone number, it must be unique.';
-			break;
+			return 'The SMS number is already assigned to another contact. If you subscribe an email address and add a phone number, it must be unique.';
 		case 10:
-			errorMessage = 'Update of contact failed.';
-			break;
+			return 'Update of contact failed.';
 		case 11:
-			errorMessage = 'Invalid phone number.';
-			break;
+			return 'Invalid phone number.';
 		case 12:
-			errorMessage = 'Internal error.';
-			break;
+			return 'Internal error.';
 		case 30:
-			errorMessage = 'The email address is blocked and cannot be used for subscription.';
-			break;
+			return 'The email address is blocked and cannot be used for subscription.';
 		case 31:
-			errorMessage = 'SmartTags are only assigned by the system.';
-			break;
+			return 'SmartTags are only assigned by the system.';
 		case 32:
-			errorMessage = 'You must specify either an email address or a SMS number.';
-			break;
+			return 'You must specify either an email address or a SMS number.';
 		case 401:
-			errorMessage = 'Contact not found.';
-			break;
+			return 'Contact not found.';
 		case 402:
-			errorMessage = 'Opt-in process not found.';
-			break;
+			return 'Opt-in process not found.';
 		case 403:
-			errorMessage = 'Tag not found.';
-			break;
+			return 'Tag not found.';
 		case 507:
-			errorMessage =
-				'You tried to add an email address to a contact that is already assigned to another contact.';
-			break;
+			return 'You tried to add an email address to a contact that is already assigned to another contact.';
 		default:
-			errorMessage = `Something went wrong. Please try again later. Error: ${error}`;
-			break;
+			return `Something went wrong. Please try again later. Error: ${error}`;
 	}
-	return errorMessage;
 }
 
 module.exports = adjustErrorMessage;
