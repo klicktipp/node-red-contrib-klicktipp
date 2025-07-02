e0f0c9a271946131e1f5feda74c2539bb004b659
Olex2324 <kovalenko.odev@gmail.com>
2025-07-02 14:01:33 +0300

    feat(update node): change new email to new email address

--- nodes/utils/transformers/prepareUpdateSubscriberData.js
+++ nodes/utils/transformers/prepareUpdateSubscriberData.js
@@ -1,14 +1,14 @@
 /**
  * Prepares the data object for updating a subscriber by filtering valid fields and including optional new email and SMS number.
  *
- * @param {string} newEmail - The new email address for the subscriber.
+ * @param {string} newEmailAddress - The new email address for the subscriber.
  * @param {string} [newSmsNumber=''] - The optional new SMS number for the subscriber.
  * @param {Object} [fields={}] - An object containing additional custom fields for the subscriber.
  * @returns {Object} The prepared update subscriber data object containing the new email, and optionally, the new SMS number and filtered fields.
  */
-function prepareUpdateSubscriberData(newEmail = '', newSmsNumber = '', fields = {}) {
+function prepareUpdateSubscriberData(newEmailAddress = '', newSmsNumber = '', fields = {}) {
 	const data = {
-		newemail: newEmail.trim(),
+		newemail: newEmailAddress.trim(),
 	};
 
 	// Add optional SMS number if provided
